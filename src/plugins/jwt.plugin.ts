f2f07c1398475bb8939f0940de10cf3fe0e9c5a2
krystian john dumapit <23546777+Kryuzaki18@users.noreply.github.com>
2026-07-10 23:57:05 +0800

    chore: update to shared constant

--- src/plugins/jwt.plugin.ts
+++ src/plugins/jwt.plugin.ts
@@ -1,12 +1,13 @@
 import fp from 'fastify-plugin';
 import fastifyJwt from '@fastify/jwt';
 import { FastifyReply, FastifyRequest } from 'fastify';
+import { STORAGE_KEYS } from '../constants/storage.constant';
 
 export default fp(async (app) => {
   await app.register(fastifyJwt, {
     secret: app.config.JWT_SECRET,
     cookie: {
-      cookieName: 'access_token',
+      cookieName: STORAGE_KEYS.ACCESS_TOKEN,
       signed: false,
     },
   });
